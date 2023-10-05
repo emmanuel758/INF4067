6fc08c710c6528cc3da7670cdd3356a13a9e9a14
emmanuel758 <emmanuelleuna758@gmail.com>
2023-10-05 22:24:26 +0100

    Duplication du code de la premier approche

---
+++ devoir-1/LEUNA FIENKAK NKEHEUP - 20U2698/Code/Abstract Factory/ProduitA1.java
@@ -0,0 +1,9 @@
+public class ProduitA1 extends ProduitA {
+
+    @Override
+    public void methodA() {
+        System.out.println("Je suis un produit de type A1");
+        System.out.println("ProduitA1.methodA()");
+    }
+
+}
---
+++ devoir-1/LEUNA FIENKAK NKEHEUP - 20U2698/Code/Abstract Factory/ProduitA2.java
@@ -0,0 +1,9 @@
+public class ProduitA2 extends ProduitA {
+
+    @Override
+    public void methodA() {
+        System.out.println("Je suis un produit de type A2");
+        System.out.println("ProduitA2.methodA()");
+    }
+
+}
